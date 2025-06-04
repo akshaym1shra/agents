692e71f3497e3d12f2152e2100feb23fd0360b62
akshaym1shra <akshay.mishra@salescode.ai>
2025-06-04 08:53:15 +0530

    Add a way to pass a list of words to be excluded during interruption handeling
    Remove words from interruption
    Update package path
    Pass ignore_interrupt_list as an Arg in livekit-agent

--- livekit-agents/livekit/agents/voice/agent_session.py
+++ livekit-agents/livekit/agents/voice/agent_session.py
@@ -57,6 +57,7 @@ class VoiceOptions:
     max_tool_steps: int
     user_away_timeout: float | None
     min_consecutive_speech_delay: float
+    ignore_interrupt_list: list[str]
 
 
 Userdata_T = TypeVar("Userdata_T")
@@ -132,6 +133,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
         min_consecutive_speech_delay: float = 0.0,
         conn_options: NotGivenOr[SessionConnectOptions] = NOT_GIVEN,
         loop: asyncio.AbstractEventLoop | None = None,
+        ignore_interrupt_list: list[str] = [],
     ) -> None:
         """`AgentSession` is the LiveKit Agents runtime that glues together
         media streams, speech/LLM components, and tool orchestration into a
@@ -194,6 +196,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
                 stt, llm, and tts.
             loop (asyncio.AbstractEventLoop, optional): Event loop to bind the
                 session to. Falls back to :pyfunc:`asyncio.get_event_loop()`.
+            ignore_interrupt_list (list[str]): List of words to ignore as interruptions.
         """
         super().__init__()
         self._loop = loop or asyncio.get_event_loop()
@@ -215,6 +218,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
             max_tool_steps=max_tool_steps,
             user_away_timeout=user_away_timeout,
             min_consecutive_speech_delay=min_consecutive_speech_delay,
+            ignore_interrupt_list=ignore_interrupt_list,
         )
         self._conn_options = conn_options or SessionConnectOptions()
         self._started = False
