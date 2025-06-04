e28914ad8c0607afbb734b7f6f7a7882b1e42f7b
akshaym1shra <akshay.mishra@salescode.ai>
2025-06-04 08:53:15 +0530

    Add a way to pass a list of words to be excluded during interruption handeling
    Remove words from interruption
    Update package path
    Pass ignore_interrupt_list as an Arg in livekit-agent

--- livekit-agents/livekit/agents/voice/agent_session.py
+++ livekit-agents/livekit/agents/voice/agent_session.py
@@ -82,6 +82,7 @@ class VoiceOptions:
     min_consecutive_speech_delay: float
     use_tts_aligned_transcript: NotGivenOr[bool]
     preemptive_generation: bool
+    ignore_interrupt_list: list[str]
 
 
 Userdata_T = TypeVar("Userdata_T")
@@ -167,6 +168,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
         loop: asyncio.AbstractEventLoop | None = None,
         # deprecated
         agent_false_interruption_timeout: NotGivenOr[float | None] = NOT_GIVEN,
+        ignore_interrupt_list: list[str] = [],
     ) -> None:
         """`AgentSession` is the LiveKit Agents runtime that glues together
         media streams, speech/LLM components, and tool orchestration into a
@@ -248,6 +250,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
                 stt, llm, and tts.
             loop (asyncio.AbstractEventLoop, optional): Event loop to bind the
                 session to. Falls back to :pyfunc:`asyncio.get_event_loop()`.
+            ignore_interrupt_list (list[str]): List of words to ignore as interruptions.
         """
         super().__init__()
         self._loop = loop or asyncio.get_event_loop()
@@ -279,6 +282,7 @@ class AgentSession(rtc.EventEmitter[EventTypes], Generic[Userdata_T]):
             min_consecutive_speech_delay=min_consecutive_speech_delay,
             preemptive_generation=preemptive_generation,
             use_tts_aligned_transcript=use_tts_aligned_transcript,
+            ignore_interrupt_list=ignore_interrupt_list,
         )
         self._conn_options = conn_options or SessionConnectOptions()
         self._started = False
